1a7c512f71851e85db0f404c13d221ca984c7c79
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-04 22:56:52 +0530

    first commit

---
+++ src/java_structure/CoffeeCraze.java
@@ -0,0 +1,20 @@
+package java_structure;
+
+
+import java.util.Scanner;
+
+public class CoffeeCraze {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pls enter season of the year");
+        String season = scanner.nextLine();
+
+        System.out.println("Pls enter a whole number");
+        int number = scanner.nextInt();
+
+        System.out.println("On one fine "+season+" day, I drink a minimum of "+number+" cups of Coffee!");
+    }
+}
+
